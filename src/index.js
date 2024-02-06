d2bdcae963059998f58709ae045de3823bbd44ac
Pavel <wolwerine@yandex.ru>
2024-02-06 19:10:03 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById("cnvs");
+
+const gameState = {};
+
+function queueUpdates(numTicks) {
+    for (let i = 0; i < numTicks; i++) {
+        gameState.lastTick = gameState.lastTick + gameState.tickLength
+        update(gameState.lastTick)
+    }
+}
+
+function draw(tFrame) {
+    const context = canvas.getContext('2d');
+
+    // clear canvas
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    // draw
+}
+
+function update(tick) {
+
+}
+
+function run(tFrame) {
+    gameState.stopCycle = window.requestAnimationFrame(run)
+
+    const nextTick = gameState.lastTick + gameState.tickLength
+    let numTicks = 0
+
+    if (tFrame > nextTick) {
+        const timeSinceTick = tFrame - gameState.lastTick
+        numTicks = Math.floor(timeSinceTick / gameState.tickLength)
+    }
+    queueUpdates(numTicks)
+    draw(tFrame)
+    gameState.lastRender = tFrame
+}
+
+function stopGame(handle) {
+    window.cancelAnimationFrame(handle);
+}
+
+function setup() {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    gameState.lastTick = performance.now()
+    gameState.lastRender = gameState.lastTick
+    gameState.tickLength = 15 //ms
+
+}
+
+setup();
+run();
