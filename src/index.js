cf4864a9f9c6fe6545c7c90782ba086ffc09b311
nenapisannoe <58001221+nenapisannoe@users.noreply.github.com>
2024-02-09 13:44:48 +0300

    colliding with walls

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import Rectangle from "./rectangle";
 
+const canvasWidth = window.innerWidth; 
+const canvasHeight = window.innerHeight; 
+
 const canvas = document.getElementById("cnvs");
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const rect = new Rectangle(getRandomInt(0, canvasWidth),getRandomInt(0, canvasHeight),20,20, 1, 0);
+
 const gameState = {rects:
-        [new Rectangle(10,10,20,20, 1, 0),
-            new Rectangle(500,10,20,20, -1, 0)]
+        [new Rectangle(getRandomInt(0, canvasWidth),getRandomInt(0, canvasHeight),20,20, 1, 0),
+            new Rectangle(getRandomInt(0, canvasWidth),getRandomInt(0, canvasHeight),20,20, -1, 0),
+            new Rectangle(getRandomInt(0, canvasWidth),getRandomInt(0, canvasHeight),20,20, -1, 0),
+            new Rectangle(getRandomInt(0, canvasWidth),getRandomInt(0, canvasHeight),20,20, -1, 0),
+            new Rectangle(getRandomInt(0, canvasWidth),getRandomInt(0, canvasHeight),20,20, -1, 0),
+            new Rectangle(getRandomInt(0, canvasWidth),getRandomInt(0, canvasHeight),20,20, -1, 0)
+        ]
 };
 
 function queueUpdates(numTicks) {
@@ -14,18 +28,27 @@ function queueUpdates(numTicks) {
     }
 }
 
+
+
 function draw(tFrame) {
     const context = canvas.getContext('2d');
+
+    function drawRectangle(rect, color) {
+        context.beginPath();
+        context.rect(rect.x, rect.y, rect.w, rect.h);
+        context.fillStyle = color;
+        context.fill();
+    }
 
     // clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height)
 
     // draw
     gameState.rects.forEach(r=>{
-        context.beginPath()
-        context.rect(r.x, r.y, r.w, r.h)
-        context.fill()
+        drawRectangle(r, "BLack")
     })
+
+    drawRectangle(rect, "Green");
 
 }
 
@@ -34,8 +57,30 @@ function update(tick) {
     gameState.rects.forEach(r=>{
         r.x += r.vx
         r.y += r.vy
+            
+        
+        if (r.x <= 0 || r.x + r.w >= canvasWidth) {
+            r.vx *= -1; 
+        }
+
+        if (r.y <= 0 || r.y + r.h >= canvasHeight) {
+            r.vy *= -1; 
+        }
 
     })
+
+
+    rect.x += rect.vx
+    rect.y += rect.vy
+
+    if (rect.x <= 0 || rect.x + rect.w >= canvasWidth) {
+        rect.vx *= -1; 
+    }
+
+    if (rect.y <= 0 || rect.y + rect.h >= canvasHeight) {
+        rect.vy *= -1; 
+    }
+
 }
 
 function run(tFrame) {
